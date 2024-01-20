0c09db936272367b3ad933baa67aca68c1421ffa
jasmehsoccer <70823994+jasmehsoccer@users.noreply.github.com>
2024-01-19 18:04:20 -0800

    Set up the waves for the game
     - Set up coroutines
     - Generated Enemy Path Script
     TO DO:
     - Finish Enemy pathing script
     - Set up script and game object

--- Assets/C# Try/enemySpawner.cs
+++ Assets/C# Try/enemySpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,42 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] bool looping = false;
     // Start is called before the first frame update
     // Make a blank C# class and create Character creator class: Write a message, "Please fill out form". Make a class called Character Creator. Make many private variables. Create getters and setters. Make a consructor. 
-    void Start()
+    IEnumerator Start()
     {
-        
-        
+        do
+        {
+           
+            yield return StartCoroutine(SpawnAllWaves());
+
+        }
+        while (looping);
     }
 
-    // Update is called once per frame
     
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = 0; waveIndex < waveConfig.Count; waveIndex++) 
+        {
+            var currentWave = waveConfig[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for (int enemyCounter = 0; enemyCounter < currentWave.GetNumSpawn();enemyCounter++)
+        {
+            var newEnemy = Instantiate(currentWave.GetEnemyPrefab(), currentWave.GetWaypoint()[0].transform.position, Quaternion.identity) ;
+            newEnemy.GetComponent<EnemyPathing>().setWaveConfig(currentWave);
+            yield return new WaitForSeconds(currentWave.GetSpawnWait());
+        }
+    }
+
+
+
+    // Update is called once per frame
+
 }
+
