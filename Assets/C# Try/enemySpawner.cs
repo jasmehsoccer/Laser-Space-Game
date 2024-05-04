35a23f1c61e4427099945971f711255d6ac1bb59
jasmehsoccer <70823994+jasmehsoccer@users.noreply.github.com>
2024-05-03 17:59:21 -0700

    Adding loop for enemies.
     - Working on some bugs
     - Need to add enemy looping

--- Assets/C# Try/enemySpawner.cs
+++ Assets/C# Try/enemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] List<waveConfig> waveConfig;
     [SerializeField] int startWave = 0;
     [SerializeField] bool looping = false;
+    [SerializeField] float nextWaveCountdown = 5f;
     // Start is called before the first frame update
     // Make a blank C# class and create Character creator class: Write a message, "Please fill out form". Make a class called Character Creator. Make many private variables. Create getters and setters. Make a consructor. 
     IEnumerator Start()
@@ -27,6 +28,7 @@ public class EnemySpawner : MonoBehaviour
         for (int waveIndex = 0; waveIndex < waveConfig.Count; waveIndex++) 
         {
             var currentWave = waveConfig[waveIndex];
+
             yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
 
 
@@ -35,10 +37,16 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnAllEnemiesInWave(waveConfig currentWave)
     {
-        for (int enemyCounter = 0; enemyCounter < currentWave.GetNumSpawn();enemyCounter++)
+        for (int enemyCounter = 0; enemyCounter <= currentWave.GetNumSpawn();enemyCounter++)
         {
+            if (enemyCounter == currentWave.GetNumSpawn())
+            {
+                Debug.Log("Starting Next Wave Timer");
+                yield return new WaitForSeconds(nextWaveCountdown);
+            }
             var newEnemy = Instantiate(currentWave.GetEnemyPrefab(), currentWave.GetWaypoint()[0].transform.position, Quaternion.identity) ;
             newEnemy.GetComponent<EnemyPathing>().setWaveConfig(currentWave);
+            
             yield return new WaitForSeconds(currentWave.GetSpawnWait());
         }
     }
